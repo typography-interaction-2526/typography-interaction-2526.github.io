8d713e6763646f74df24fb5be6abfcc37487c379
Michael Fehrenbach <mf@michaelfehrenbach.com>
2025-08-06 23:29:58 -0400

    Start adding `markdown-it`

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,5 +1,11 @@
 import webC from '@11ty/eleventy-plugin-webc'
 
+import markdownIt from 'markdown-it'
+import markdownItAbbr from 'markdown-it-abbr'
+import markdownItAttrs from 'markdown-it-attrs'
+import markdownItDeflist from 'markdown-it-deflist'
+import markdownItHeaderSections from 'markdown-it-header-sections'
+
 export default (eleventyConfig) => {
 	// Setup.
 	eleventyConfig.addPlugin(webC, { components: 'components/**/*.webc' })
@@ -8,6 +14,34 @@ export default (eleventyConfig) => {
 		language: 'js',
 	})
 	eleventyConfig.addGlobalData('layout', 'base.webc')
+
+	const markdownOptions = {
+		html: true,
+		breaks: true,
+		linkify: true,
+		typographer: true,
+	}
+
+	const markdown = markdownIt(markdownOptions)
+		.use(markdownItAbbr)
+		.use(markdownItDeflist)
+		.use(markdownItHeaderSections)
+		})
+		.use(markdownItAttrs)
+		.use(markdown => {
+			markdown.renderer.rules.fence = (tokens, idx, options, env, slf) => {
+				const token = tokens[idx]
+				return `<pre ${slf.renderAttrs(token)}><code class="language-${token.info.trim()}">${markdown.utils.escapeHtml(token.content)}</code></pre>`
+			}
+		})
+
+	// Convert HTML comments to curly brackets for `markdownItAttrs` to pick up.
+	eleventyConfig.addPreprocessor('commentsToCurlies', '.md', (data, content) =>
+		// Only match `.class`…, `#id`…, `data`…, `style`… so example/other comments aren’t transformed.
+		content.replace(/<!--\s*(\.(?:[\s\S]*?)|#(?:[\s\S]*?)|data(?:[\s\S]*?)|style(?:[\s\S]*?))\s*-->$/gm, '{ $1 }'),
+	)
+
+	eleventyConfig.setLibrary('md', markdown)
 
 	// Remainder setup.
 	return {
