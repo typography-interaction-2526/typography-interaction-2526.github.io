e926c0a9af7b57c5078b376bcdaad239c7be63d2
Michael Fehrenbach <mf@michaelfehrenbach.com>
2025-08-06 22:09:54 -0400

    Add `.webc`

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,10 +1,16 @@
+import webC from '@11ty/eleventy-plugin-webc'
+
 export default (eleventyConfig) => {
+	// Setup.
+	eleventyConfig.addPlugin(webC, { components: 'components/**/*.webc' })
+
+	// Remainder setup.
 	return {
 		dir: {
 			input: 'content',
 			output: '_site',
 		},
-		htmlTemplateEngine: 'html',
+		htmlTemplateEngine: 'webc',
 		markdownTemplateEngine: false,
 	}
 }
