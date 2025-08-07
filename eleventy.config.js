4004f984f2d0ddbba344648e8b3c1daad7236f20
Michael Fehrenbach <mf@michaelfehrenbach.com>
2025-08-06 22:43:52 -0400

    `head`, shoulder, knees, and toes

--- eleventy.config.js
+++ eleventy.config.js
@@ -3,12 +3,20 @@ import webC from '@11ty/eleventy-plugin-webc'
 export default (eleventyConfig) => {
 	// Setup.
 	eleventyConfig.addPlugin(webC, { components: 'components/**/*.webc' })
+	eleventyConfig.setFrontMatterParsingOptions({
+		delimiters: ['<!---', '--->'],
+		language: 'js',
+	})
+	eleventyConfig.addGlobalData('layout', 'base.webc')
 
 	// Remainder setup.
 	return {
 		dir: {
 			input: 'content',
 			output: '_site',
+
+			// Relative to `input`.
+			layouts: '../layouts',
 		},
 		htmlTemplateEngine: 'webc',
 		markdownTemplateEngine: false,
