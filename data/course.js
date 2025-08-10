94105ae6d6ec9cd1056a5f26a2adadd394a26ea1
Michael Fehrenbach <mf@michaelfehrenbach.com>
2025-08-10 00:08:54 -0400

    Stub out channels

--- data/course.js
+++ data/course.js
@@ -3,5 +3,14 @@ export default {
 		fall: 'https://courses.newschool.edu/courses/PMCD5001',
 		spring: 'https://courses.newschool.edu/courses/PMCD5002',
 	},
+	channel: {
+		figma: 'https://www.figma.com/files/team/',
+		github: 'https://github.com/typography-interaction-2526',
+		google: 'https://drive.google.com/drive/folders/',
+		slack: 'https://mpscd.slack.com/archives/',
+		submit: 'https://docs.google.com/forms/d/e/',
+		zoom: 'https://NewSchool.zoom.us/j/',
+		zoomAlt: 'https://newschool.zoom.us/my/mfehrenbach',
+	},
 	title: 'Typography & Interaction, 2025–2026',
 }
