d4645ae951858df652ecdf937d80e9778607ef8f
Michael Fehrenbach <mf@michaelfehrenbach.com>
2025-08-21 22:27:37 -0400

    Only actual paragraphs

--- eleventy.config.js
+++ eleventy.config.js
@@ -73,11 +73,11 @@ export default (eleventyConfig) => {
 
 						// Prevent orphans at the end of a block/paragraph (not just token).
 						!children?.slice(index + 1).some((token) => token.type === 'text' && token.content.trim()) &&
-							children?.map((token) => token.type === 'text' ? token.content : '').join('').length > 64 &&
+							 /[.!?…:;)]$/.test(children?.map((token) => token.type === 'text' ? token.content : '').join('').trim()) &&
 								(child.content = child.content.replace(/(\S+)\s+(\S+)(?=\s*$)/g,
 									(match, prevWord, lastWord, offset, string) =>
 										/^\s*$/.test(string.slice(offset + match.length)) && prevWord.length + lastWord.length <= 20
-											? `${prevWord}🍕${lastWord}`
+											? `${prevWord}\u00A0${lastWord}`
 											: match,
 								))
 					}
