bf46e76b830f72404790856e1ab4fa1556f0b49c
Michael Fehrenbach <mf@michaelfehrenbach.com>
2026-01-27 16:42:38 -0500

    Fake some console warnings/errors

---
+++ content/topic/dev-tools/console/script.js
@@ -0,0 +1,21 @@
+console.log('Hello, world!')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// null error.
+setTimeout(() => {
+	document.getElementById('error').addEventListener('click', () => console.log(error))
+}, 1)
