e5d038c0456523e543be580158a7016fa6fe7d5a
Michael Fehrenbach <mf@michaelfehrenbach.com>
2026-01-28 18:52:01 -0500

    Update variable name

--- content/topic/javascript/external/script.js
+++ content/topic/javascript/external/script.js
@@ -1,6 +1,6 @@
 // Set up a variable for our button!
-let button = document.querySelector('#example') // Any CSS selector.
+let alertButton = document.querySelector('#example') // Any CSS selector.
 
-button.onclick = () => { // “Listen” for clicks.
+alertButton.onclick = () => { // “Listen” for clicks.
 	alert('The button was clicked!') // Pop an alert!
 }
