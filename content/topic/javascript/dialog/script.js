6f8065e17d73dfb19d6ed22db3b25041514f673c
Michael Fehrenbach <mf@michaelfehrenbach.com>
2026-01-29 14:05:09 -0500

    Fix modal one

--- content/topic/javascript/dialog/script.js
+++ content/topic/javascript/dialog/script.js
@@ -1,7 +1,7 @@
 // Similar to before, setting up variables.
 let modalButton = document.querySelector('#modal') // The thing we’re clicking.
 let modalDialog = document.querySelector('#dialog') // Now one for our `dialog`.
-let closeButton = modalDialog.querySelector('.close') // Only looking within `modalDialog`.
+let closeButton = modalDialog.querySelector('button') // Only looking within `modalDialog`.
 
 modalButton.addEventListener('click', () => { // “Listen” for clicks.
 	modalDialog.showModal() // This opens it up.
